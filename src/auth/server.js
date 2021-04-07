2c8de1e7e630b9ccde14f6f5e31710383f52b1d7
benji011 <wolfnun011@gmail.com>
2021-04-07 15:14:17 +0900

    fix: Added register POST endpoint connected with Supabase

--- src/auth/server.js
+++ src/auth/server.js
@@ -1,21 +1,72 @@
+const { createClient } = require('@supabase/supabase-js')
+
 require('dotenv').config()
 
-const jwt = require('jsonwebtoken')
 const express = require('express')
+const bcrypt = require('bcrypt')
 const authentication = require('../middleware/authentication')
-const { AUTH_PORT } = require('../constants/constants')
+const {
+  AUTH_PORT,
+  STATUS_BAD_GATEWAY,
+  STATUS_INTERNAL_SERVER_ERROR,
+  STATUS_CREATED,
+  SUPABASE_URL,
+  SUPABASE_KEY,
+} = require('../constants/constants')
 
+// Initialize supabase client
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+// Initialize as an Express app
 const app = express()
-
 app.use(express.json())
+
+/**
+ * Register user account
+ */
+app.post('/register', async (req, res) => {
+  try {
+    const salt = 16
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const user = {
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashedPassword,
+    }
+    const { data: users, error } = await supabase
+      .from('users')
+      .select('email')
+      .eq('email', user.email)
+    if (error) {
+      res.sendStatus(STATUS_INTERNAL_SERVER_ERROR)
+    }
+    if (users.length > 0) {
+      res.json({
+        error: `A user with the email address "${user.email}" already exists.`,
+      })
+    } else {
+      const { error } = await supabase.from('users').insert([user])
+      if (error) {
+        res.sendStatus(STATUS_INTERNAL_SERVER_ERROR)
+      } else {
+        res.sendStatus(STATUS_CREATED)
+      }
+    }
+  } catch {
+    res.sendStatus(STATUS_BAD_GATEWAY)
+  }
+})
 
 /**
  * Authenticate user then get a JWT
  * TODO: Salt hashed passwords with bcrypt later
  */
 app.post('/login', (req, res) => {
-  const username = req.body.username
-  const user = { name: username }
+  const email = req.body.email
+  const password = req.body.password
+  const user = { email: email }
   const jwtSignedAccessToken = authentication.generateAccessToken(user)
   const jwtSignedRefreshToken = authentication.generateRefreshToken(user)
   res.json({
