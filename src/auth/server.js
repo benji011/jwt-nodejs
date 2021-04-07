35d76850498f43640fb7519121f3540ceea9aedf
Benjamin Lo <wolfnun011@gmail.com>
2021-04-07 16:01:09 +0900

    Added `/register` endpoint & validation for login & password checking using bcrypt (#1)
    * fix: Added register POST endpoint connected with Supabase
    
    * fix: registeration & login using bcrypt to compare

--- src/auth/server.js
+++ src/auth/server.js
@@ -1,27 +1,102 @@
+const { createClient } = require('@supabase/supabase-js')
+const { _ } = require('lodash')
+
 require('dotenv').config()
 
-const jwt = require('jsonwebtoken')
 const express = require('express')
+const bcrypt = require('bcrypt')
 const authentication = require('../middleware/authentication')
-const { AUTH_PORT } = require('../constants/constants')
+const {
+  AUTH_PORT,
+  STATUS_BAD_GATEWAY,
+  STATUS_INTERNAL_SERVER_ERROR,
+  STATUS_CREATED,
+  SUPABASE_URL,
+  SUPABASE_KEY,
+} = require('../constants/constants')
 
+// Initialize supabase client
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+// Initialize as an Express app
 const app = express()
-
 app.use(express.json())
 
 /**
- * Authenticate user then get a JWT
- * TODO: Salt hashed passwords with bcrypt later
+ * Register user account
  */
-app.post('/login', (req, res) => {
-  const username = req.body.username
-  const user = { name: username }
-  const jwtSignedAccessToken = authentication.generateAccessToken(user)
-  const jwtSignedRefreshToken = authentication.generateRefreshToken(user)
-  res.json({
-    accessToken: jwtSignedAccessToken,
-    refreshToken: jwtSignedRefreshToken,
-  })
+app.post('/register', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(16)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const user = {
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashedPassword,
+    }
+    const { data: users, error } = await supabase
+      .from('users')
+      .select('email')
+      .eq('email', user.email)
+    if (error) {
+      res.sendStatus(STATUS_INTERNAL_SERVER_ERROR)
+    }
+    if (users.length > 0) {
+      res.json({
+        error: `A user with the email address "${user.email}" already exists.`,
+      })
+    } else {
+      const { error } = await supabase.from('users').insert([user])
+      if (error) {
+        res.sendStatus(STATUS_INTERNAL_SERVER_ERROR)
+      } else {
+        res.sendStatus(STATUS_CREATED)
+      }
+    }
+  } catch {
+    res.sendStatus(STATUS_BAD_GATEWAY)
+  }
+})
+
+/**
+ * Authenticate user then get a JWT
+ */
+app.post('/login', async (req, res) => {
+  try {
+    const email = req.body.email
+    const salt = await bcrypt.genSalt(16)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    const user = { email: email, password: hashedPassword }
+
+    const { data: data } = await supabase
+      .from('users')
+      .select('password')
+      .eq('email', email)
+    const supabasePassword = _.map(data, 'password')[0]
+
+    // Compare the hashed password with what we found from the DB with that email
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      supabasePassword
+    )
+    if (validPassword) {
+      // Assuming authentication was completed, generate then return an access and refresh token.
+      const jwtSignedAccessToken = authentication.generateAccessToken(user)
+      const jwtSignedRefreshToken = authentication.generateRefreshToken(user)
+      res.json({
+        accessToken: jwtSignedAccessToken,
+        refreshToken: jwtSignedRefreshToken,
+      })
+    } else {
+      res.json({
+        message: 'Either the password is wrong or something else went wrong',
+      })
+    }
+  } catch {
+    res.sendStatus(STATUS_BAD_GATEWAY)
+  }
 })
 
 /**
